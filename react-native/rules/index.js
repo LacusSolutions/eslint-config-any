54d1d707f87fdc052c87800b7f7e838d988ba0a7
juliolmuller <juliolmuller@outlook.com>
2025-03-07 23:46:57 -0300

    chore: drop support to React Native

--- react-native/rules/index.js
+++
@@ -1,8 +0,0 @@
-const requirePlugins = require('../../utils/require-plugins')
-const pluginsRules = requirePlugins(`${__dirname}/plugins`)
-const overrides = require('./overrides')
-
-module.exports = {
-  ...pluginsRules,
-  ...overrides, // <<< MUST BE THE LAST ONE
-}
