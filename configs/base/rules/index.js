00a8bfa31f7c598cd3dcc2e029d1311029ddbe71
juliolmuller <juliolmuller@outlook.com>
2025-03-14 00:06:53 -0300

    feat(base): set some strict rules

--- configs/base/rules/index.js
+++ configs/base/rules/index.js
@@ -1,4 +1,18 @@
 export default {
+  // https://eslint.org/docs/rules/eqeqeq
+  eqeqeq: 'error',
+
+  // https://eslint.org/docs/rules/func-style
+  'func-style': ['error', 'declaration'],
+
+  // https://eslint.org/docs/rules/no-console
+  'no-console': [
+    'error',
+    {
+      allow: [console.error.name, console.info.name, console.warn.name],
+    },
+  ],
+
   // https://eslint.org/docs/rules/no-unused-vars
   'no-unused-vars': [
     'error',
@@ -8,4 +22,13 @@ export default {
       ignoreRestSiblings: true,
     },
   ],
+
+  // https://eslint.org/docs/rules/prefer-arrow-callback
+  'prefer-arrow-callback': 'error',
+
+  // https://eslint.org/docs/rules/prefer-const
+  'prefer-const': 'error',
+
+  // https://eslint.org/docs/rules/prefer-template
+  'prefer-template': 'error',
 };
