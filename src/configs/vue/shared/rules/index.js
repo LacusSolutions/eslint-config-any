96dd6154ab6f0295ada3613b5c4f8f84b37961c4
juliolmuller <juliolmuller@outlook.com>
2025-03-23 00:52:41 -0300

    feat: override `vue/html-self-closing` rule

--- src/configs/vue/shared/rules/index.js
+++ src/configs/vue/shared/rules/index.js
@@ -8,6 +8,20 @@ export default {
     },
   ],
 
+  // https://eslint.vuejs.org/rules/vue/html-self-closing.html
+  'vue/html-self-closing': [
+    'error',
+    {
+      html: {
+        void: 'always',
+        normal: 'always',
+        component: 'always',
+      },
+      svg: 'always',
+      math: 'always',
+    },
+  ],
+
   // https://eslint.vuejs.org/rules/multi-word-component-names.html
   'vue/multi-word-component-names': 'off',
 
@@ -59,73 +73,6 @@ export default {
     },
   ],
 
-  // // https://eslint.vuejs.org/rules/html-closing-bracket-newline.html
-  // 'vue/html-closing-bracket-newline': [
-  //   'error',
-  //   {
-  //     singleline: 'never',
-  //     multiline: 'always',
-  //   },
-  // ],
-  //
-  // // https://eslint.vuejs.org/rules/html-closing-bracket-spacing.html
-  // 'vue/html-closing-bracket-spacing': [
-  //   'error',
-  //   {
-  //     selfClosingTag: 'always',
-  //     startTag: 'never',
-  //     endTag: 'never',
-  //   },
-  // ],
-  //
-  // // https://eslint.vuejs.org/rules/html-comment-indent.html
-  // 'vue/html-comment-indent': ['error', 2],
-  //
-  // // https://eslint.vuejs.org/rules/html-end-tags.html
-  // 'vue/html-end-tags': 'error',
-  //
-  // // https://eslint.vuejs.org/rules/html-indent.html
-  // 'vue/html-indent': [
-  //   'error',
-  //   2,
-  //   {
-  //     attribute: 1,
-  //     baseIndent: 1,
-  //     closeBracket: 0,
-  //     alignAttributesVertically: false,
-  //     ignores: [],
-  //   },
-  // ],
-  //
-  // // https://eslint.vuejs.org/rules/html-self-closing.html
-  // 'vue/html-self-closing': [
-  //   'error',
-  //   {
-  //     svg: 'always',
-  //     math: 'always',
-  //     html: {
-  //       void: 'always',
-  //       normal: 'any',
-  //       component: 'always',
-  //     },
-  //   },
-  // ],
-  //
-  // // https://eslint.vuejs.org/rules/html-quotes.html
-  // 'vue/html-quotes': [
-  //   'error',
-  //   'double',
-  //   {
-  //     avoidEscape: false,
-  //   },
-  // ],
-  //
-  // // https://eslint.vuejs.org/rules/mustache-interpolation-spacing.html
-  // 'vue/mustache-interpolation-spacing': ['error', 'always'],
-  //
-  // // https://eslint.vuejs.org/rules/require-name-property.html
-  // 'vue/require-name-property': 'error',
-  //
   // // https://eslint.vuejs.org/rules/script-indent.html
   // 'vue/script-indent': [
   //   'error',
