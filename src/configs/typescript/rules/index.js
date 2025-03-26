600058f1cb29d4f7859ac175807f7dd4b76ccb6f
juliolmuller <juliolmuller@outlook.com>
2025-03-26 00:48:40 -0300

    feat: set `@typescript-eslint/consistent-type-imports` rule

--- src/configs/typescript/rules/index.js
+++ src/configs/typescript/rules/index.js
@@ -1,6 +1,16 @@
 import standardRules from '../../base/rules/index.js';
 
 export default {
+  // https://typescript-eslint.io/rules/consistent-type-imports
+  '@typescript-eslint/consistent-type-imports': [
+    'warn',
+    {
+      disallowTypeAnnotations: true,
+      fixStyle: 'inline-type-imports',
+      prefer: 'type-imports',
+    },
+  ],
+
   // https://typescript-eslint.io/rules/no-unused-vars
   '@typescript-eslint/no-unused-vars': standardRules['no-unused-vars'],
 };
