109864afdf94e2cc5692e396979e50bdfc33ab25
juliolmuller <juliolmuller@outlook.com>
2026-09-18 13:02:58 -0300

    style: lint source code

--- src/configs/base/plugins/regexp/index.js
+++ src/configs/base/plugins/regexp/index.js
@@ -1,11 +1,11 @@
-import regexpPlugin from 'eslint-plugin-regexp';
+import { configs as regexpPluginConfigs } from 'eslint-plugin-regexp';
 import { defineConfig } from 'eslint/config';
 
 import { JS, TS, VUE } from '../../../../utils/index.js';
 import rules from './rules/index.js';
 
 const matchingFilesPattern = [JS, TS, VUE];
-const baseRegExpConfig = regexpPlugin.configs.recommended;
+const baseRegExpConfig = regexpPluginConfigs.recommended;
 
 export default defineConfig([
   {
