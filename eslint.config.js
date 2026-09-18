fc7670a8ff93d0032cc6286fd650fce7df45355f
juliolmuller <juliolmuller@outlook.com>
2026-09-18 13:02:37 -0300

    chore(lint): define new project linting rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,18 @@
+import { defineConfig } from 'eslint/config';
+
 import jlmConfig from './dist/index.js';
 
-export default jlmConfig.node;
+export default defineConfig([
+  ...jlmConfig.node,
+  {
+    rules: {
+      'import-x/extensions': [
+        'warn',
+        'ignorePackages',
+        {
+          fix: true,
+        },
+      ],
+    },
+  },
+]);
