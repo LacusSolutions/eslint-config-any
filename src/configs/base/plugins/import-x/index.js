f40c33a5f5779ea879c3dc3835f7486952de6d99
juliolmuller <juliolmuller@outlook.com>
2026-09-23 17:39:04 -0300

    fix(import-x): fix plugin parser

--- src/configs/base/plugins/import-x/index.js
+++ src/configs/base/plugins/import-x/index.js
@@ -1,16 +1,30 @@
-import { importX } from 'eslint-plugin-import-x';
+import { createNodeResolver, importX } from 'eslint-plugin-import-x';
 import { defineConfig } from 'eslint/config';
+import fs from 'node:fs';
+import path from 'node:path';
 
 import { DTS, JS, toBlob, toDotted, TS, VUE } from '../../../../utils/index.js';
 import rules from './rules/index.js';
 
 const matchingFileBlobs = toBlob(JS, TS, DTS, VUE);
+const matchingFileExtensions = toDotted(JS, TS, DTS);
+
+const configFile = path.resolve(process.cwd(), 'tsconfig.json');
+const tsconfigExists = fs.existsSync(configFile);
 
 export default defineConfig([
   {
     files: matchingFileBlobs,
     plugins: {
       'import-x': importX,
+    },
+    settings: {
+      'import-x/resolver-next': [
+        createNodeResolver({
+          extensions: matchingFileExtensions,
+          tsconfig: tsconfigExists ? { configFile } : undefined,
+        }),
+      ],
     },
     rules,
   },
